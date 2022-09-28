a6f5c100c8837af7d544e01260206f5d151cf314
Mehmet Durgun <m.durgun019@gmail.com>
2022-09-29 00:19:25 +0300

    BeforeAfter

---
+++ src/TestBase/BeforeAfter.java
@@ -0,0 +1,47 @@
+package TestBase;
+
+public class BeforeAfter {
+   /*
+    public abstract class TestBaseBeforeClassAfterClass {
+
+
+        protected static WebDriver driver;
+
+        @BeforeClass
+        public static void setUp() {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        }
+
+        @AfterClass
+        public static void tearDown() {
+            // driver.close();
+        }
+    }
+
+
+======================================================================================
+
+
+    public abstract class TestBaseBeforeAfter {
+
+        protected WebDriver driver;
+
+        @Before
+        public void setUp() {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        }
+        @After
+        public void tearDown() {
+            // driver.close();
+
+        }
+     */
+}
+
